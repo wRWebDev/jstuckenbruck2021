7e0617a1efbb79fbd327fb3c71bbebabde6f2d91
wRWebDev <wrobertson@hd9host.com>
2022-01-09 02:38:37 +0000

    Dashboard links

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -1,12 +1,31 @@
 import Layout from '../../components/Admin/Layout/Layout'
 import { checkLogin } from '../../lib/Auth'
+import { useRouter } from 'next/router'
 
 const Dashboard = () => {
+
+    const router = useRouter()
+
     return (
         <Layout>
             
-            <h1>Dashboard</h1>
-            <h2>jstuckenbruck.com</h2>
+            <h2>Hi, Johann!</h2>
+
+            <article>
+
+                <h3 style={{marginTop: '40pt', cursor: 'pointer'}} onClick={() => router.push('/admin/edit')}>Content</h3>
+                <p>Edit background images and text for each section of the site</p>
+                <p>You'll also find contact details for your agent here.</p>
+
+                <h3 style={{marginTop: '40pt', cursor: 'pointer'}} onClick={() => router.push('/admin/schedule')}>Schedule</h3>
+                <p>Add / remove / update concerts and events here.</p>
+                <p>Enter a concert or tour's details and then add as many dates as you need to it.</p>
+            
+                <h3 style={{marginTop: '40pt', cursor: 'pointer'}} onClick={() => router.push('/admin/media')}>Media</h3>
+                <p>Deal with your photo and video gallerys.</p>
+                <p>Just a friendly reminder that it's expecting you to link to Youtube videos.</p>
+
+            </article>
 
         </Layout>
     )
